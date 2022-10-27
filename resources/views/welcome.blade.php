c5441717d09265e309c792262d5d53f3c34bf08e
Pedro Henrique Neves da Silva <ptersnow@gmail.com>
2022-10-26 21:06:58 -0400

    Adição da seção Início.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,20 +9,34 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-
+        
+        @vite('resources/css/app.css')
     </head>
     <body class="antialiased">
-        <nav class="container flex justify-center mx-auto">
+        <nav class="container bg-gray-200 flex justify-center mx-auto">
             <div>
                 <img src="" alt="">
             </div>
-            <div>
-                <a href="">Início</a>
-                <a href="">Sobre</a>
-                <a href="">Serviços</a>
-                <a href="">Depoimentos</a>
-                <a href="">Contato</a>
+            <div class="text-green-900">
+                <a href="#inicio">Início</a>
+                <a href="#sobre">Sobre</a>
+                <a href="#servicos">Serviços</a>
+                <a href="#depoimentos">Depoimentos</a>
+                <a href="#contato">Contato</a>
             </div>
-        </nav>        
+        </nav>
+
+        <section id="inicio" class="bg-gray-100">
+            <h2 class="text-green-900 text-3xl font-bold">
+                Saúde natural para os seus cabelos
+            </h2>
+            <p class="text-gray-500 text-lg">
+                Um salão exclusivo em São Paulo, especializado em tratamentos naturais.
+            </p>
+            <button class="bg-green-500 rounded h-[50px] w-[218px] text-base font-medium text-gray-100">
+                Agendar um horário
+            </button>
+        </section>
+
     </body>
 </html>
